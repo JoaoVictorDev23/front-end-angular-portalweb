733350ab522770e2ceef19e5ca676c52e1ecb943
JoaoVictorDev23 <joaovictor500@gmail.com>
2024-01-08 15:35:17 -0300

    HTML e CSS para componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
     //Rota rota vazia para nav component
-    path:'', component:NavComponent
+    path:'', component:NavComponent, children:[
+      {path: 'home', component: HomeComponent}
+    ]
   }
 ];
 
