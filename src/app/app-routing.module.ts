7113b781fd4336980e8379d2e1dd55c668fe4976
JoaoVictorDev23 <joaovictor500@gmail.com>
2024-01-09 11:13:48 -0300

    Adicionado Table para Usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { UsuarioListComponent } from './components/usuarios/usuario-list/usuario-list.component';
 
 
 const routes: Routes = [
   {
     //Rota rota vazia para nav component
     path:'', component:NavComponent, children:[
-      {path: 'home', component: HomeComponent},
-
+      {path: '', component: HomeComponent},
+      {path: 'usuarios', component: UsuarioListComponent}
+ 
     ]
   }
 ];
