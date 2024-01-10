28a91c48e9660293543385ae556179eb9023220d
JoaoVictorDev23 <joaovictor500@gmail.com>
2024-01-10 14:54:16 -0300

    Adicionado Botão para Usuario e findAll

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core';
+import { API_CONFIG } from '../config/api.config';
+import { Observable } from 'rxjs';
+import { usuario } from '../models/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(private http:HttpClient) { }
+  
+  findAll(): Observable<usuario[]>{
+    return this.http.get<usuario[]>(`${API_CONFIG.baseUrl}/usuarios`);
+  }
+}
