86f7c8125cf7ba8075dc69d677a0913d380a4c18
JoaoVictorDev23 <joaovictor500@gmail.com>
2024-01-12 16:19:33 -0300

    Validação de formulario

--- src/app/components/tarifas/tarifas-create/tarifas-create.component.ts
+++ src/app/components/tarifas/tarifas-create/tarifas-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tarifas-create',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TarifasCreateComponent implements OnInit {
 
+  inicioRota:    FormControl = new FormControl(null, [Validators.required]);
+  fimRota:       FormControl = new FormControl(null, [Validators.required]);
+  valorTarifa:   FormControl = new FormControl(null, [Validators.required]);
+  status:        FormControl = new FormControl(null, [Validators.required]);
+  usuario:       FormControl = new FormControl(null, [Validators.required]);
+  observacao:    FormControl = new FormControl(null, [Validators.required]);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    return this.inicioRota.valid && this.fimRota.valid  && this.valorTarifa.valid && this.status.valid
+    && this.usuario.valid && this.observacao.valid
   }
 
 }
